3c8575e62f6522b490b48ae1f1ca154cf1fad270
Phil BStorm <philippe.haerens@bstorm.be>
2025-11-12 09:35:12 +0100

    [Update] movie details get param from route

--- src/app/features/movie/pages/movie-details-page/movie-details-page.ts
+++ src/app/features/movie/pages/movie-details-page/movie-details-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details-page',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './movie-details-page.html',
   styleUrl: './movie-details-page.scss',
 })
-export class MovieDetailsPage {
+export class MovieDetailsPage implements OnInit {
+  private readonly _activactedRoute = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
 
+  movieId!: number;
+
+  ngOnInit(): void {
+    // this.movieId = +this._activactedRoute.snapshot.params['id'];
+
+    this._activactedRoute.params.subscribe({
+      next: (param) => {
+        this.movieId = +param['id'];
+
+        // TODO récupérer les infos
+      },
+    });
+  }
+
+  onNextMovie() {
+    this._router.navigate(['/', 'movie', this.movieId + 1]);
+  }
 }
